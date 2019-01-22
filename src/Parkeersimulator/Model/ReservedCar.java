10bf2945a9d065d9e8ac4384ce8775c46eaed5b8
Andy <andyper@hotmail.nl>
2019-01-22 14:28:23 +0100

    added reservedcar

---
+++ src/Parkeersimulator/Model/ReservedCar.java
@@ -0,0 +1,21 @@
+package Parkeersimulator.Model;
+
+import Parkeersimulator.Model.Car;
+
+import java.util.Random;
+import java.awt.*;
+
+public class ReservedCar extends Car {
+	private static final Color COLOR=Color.green;
+	
+    public ReservedCar() {
+    	Random random = new Random();
+    	int stayMinutes = (int) (60 + random.nextFloat() * 7 * 60);
+        this.setMinutesLeft(stayMinutes);
+        this.setHasToPay(false);
+    }
+    
+    public Color getColor(){
+    	return COLOR;
+    }
+}
